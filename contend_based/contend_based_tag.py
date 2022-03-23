52baa953a02f498e260140057303d6c518efd3c7
고주희 <budludl@naver.com>
2022-03-23 14:50:39 +0900

    REFACTOR : 태그 기반 콘텐츠 필터링 공연 추천 [S06P22A301-112]

--- contend_based/contend_based_tag.py
+++ contend_based/contend_based_tag.py
@@ -5,74 +5,90 @@ from sqlalchemy.sql.functions import percent_rank
 from ast import literal_eval
 from sklearn.feature_extraction.text import  CountVectorizer
 from sklearn.metrics.pairwise import  cosine_similarity
+import json
 
 warnings.filterwarnings('ignore')
 
 
 def content_based_tag_to_csv():
-    performances = pd.read_csv()  # csv 데이터 읽어올 곳 경로 작성
-    performances_df = performances[['id', 'tags']]  # 태그 기반 추천(tags는 나중에 맞는 이름으로 변경)
-    print(performances)  # 입력받은 데이터 출력
+    with open("C:/Users/SSAFY/Downloads/UI/test.json", "r", encoding="utf8") as f:
+        json_data = json.load(f)
 
-    # 태그는 List임 -> ' '로 나눠지게 처리
-    performances_df['tags'] = performances_df['tags'].apply(literal_eval())  # {}를 각각 나눠주기
-    performances_df['tags'] = performances_df['tags'].apply(lambda x: [y['tag_name'] for y in x])
-    # 태그 가중치 높은 3개만 분석할 것 -> 변경 가능
-    performances_df['tags'] = performances_df['tags'].sort_values('weight')[:3]
+    print(json_data)
 
-    performances_df['tags_literal'] = performances_df['tags'].apply(lambda x: (' ').join(x))
-    count_vec = CountVectorizer(min_df=0, ngram_range=(1, 2))
-    tag_mat = count_vec.fit_transform(performances_df['tags_literal'])
+    keys = list(json_data.keys()) # ['193', '179', '145', '197', '148', '163', '190', '171', '154', '149', '155', '192', '174', '146', '144', '182', '156', '150', '172', '142', '151', '187', '186', '175', '167', '176', '177', '166', '181', '180', '165', '153', '188', '160', '195', '173']
+    print(list(json_data.keys()))
 
-    # 평점을 오름차순으로 짤라보자
+    # 0을 뺀 새로운 dictionary 만들기
+    e = {}
+    for i in range(len(keys)):
+        e[keys[i]] = json_data[keys[i]]['0'] # {'i': {'데스': 41.7049521733, '데스노트': 40.6126831601, '노트': 34.9861600762, '데': 23.2812831277, '스': 22.6366570209, '라이토': 20.3063415801, '미사': 16.9219513167, '컬': 15.9068400688, '뮤지컬': 15.2950385276, '뮤': 15.2950385276}}
+    print(e)
 
-    # 그 담은 리뷰 개수 몇 개 이상
+    # DataFrame 만들기
+    e_df = pd.DataFrame.from_dict(e)
+    # NaN을 0으로 바꾸기
+    e_df = e_df.replace(np.nan, 0)
+    # print(e_df)
 
-    # 코사인 유사도 구함
-    tags_sim = cosine_similarity(tag_mat, tag_mat)
-    print(tags_sim)
+    # df 전치시키기
+    e_df = pd.DataFrame.transpose(e_df)
+    # e_df.index(keys)
+    print(e_df)
+
+
+    # 코사인 유사도 구하기
+    tags_sim = cosine_similarity(e_df, e_df)
+    # print(tags_sim.shape)
+    # print(tags_sim)
 
     # 구한 거 정렬
     tags_sim_sort_ind = tags_sim.argsort()[:, ::-1]
+    # tags_sim_sort_ind.index(keys)
     print(tags_sim_sort_ind)
 
     # 코사인 유사도 저장
-    path = ""  # 저장할 경로
-    dataframe = pd.DataFrame(tags_sim_sort_ind)
-    dataframe.to_csv(path, header=False, index=False)
+    path = "C:/Users/SSAFY/Downloads/UI/output.csv"  # 저장할 경로
+    tag_csv = pd.DataFrame(tags_sim_sort_ind, index=keys)
+    tag_csv.to_csv(path, header=True, index=True)
+
     pass
 
 
-# 하나의 공연에 대한 관련 공연
-def find_sim_performance(df, sorted_ind, pid, top_n=15):
-    performance_id = df[df['id'] == pid]
+def find_recommend(pid) :
+    # tags_sim_sort_ind 갖고옴 : index 는 공연 id
+    tags_sim_sort_ind = pd.read_csv('C:/Users/SSAFY/Downloads/UI/output.csv', index_col=0)
+    # print(tags_sim_sort_ind)
 
-    performance_index = performance_id.index.values
-    similar_indexes = sorted_ind[performance_index, :(top_n*2)]
-
-    similar_indexes = similar_indexes[similar_indexes != pid]
-    similar_indexes = similar_indexes.reshape(-1)
-    return df.iloc[similar_indexes]
+    # 함수 호출
+    find_sim_performance(tags_sim_sort_ind, pid)
 
 
-# 공연 여러 개 입력 받음
-def find_sim_performances(perform, performances_df, tags_sim_sort_ind):
-    p = []
-    for i in perform :
-        p.append(find_sim_performance(performances_df, tags_sim_sort_ind, i))
+def find_sim_performance(sorted_ind, pid, top_n=16):
+    # performance_id 는 공연 id를 str 으로 변환한 값
+    performance_id = str(pid)
 
-    return p
+    # 원하는 개수만큼만 뽑기
+    similar_indexes = sorted_ind.loc[pid, : str(top_n)]
+    print(similar_indexes.index)
 
-# 공연 간 유사도 구하기
-# -> df, sorted_ind 등을 태그, 장르, 배우로 바꿀 수 있는 것!
-# 현재까지는 평균 별점, 리뷰 수 고려하지 x
+    # 0번째는 자기 자신이므로 없앰
+    similar_indexes = similar_indexes[1:]
+    print(similar_indexes)
+
+    # 공연 번호로 변환
+    keys = pd.DataFrame(sorted_ind.index)
+    similar_indexes_id = keys.iloc[similar_indexes]
+    print(similar_indexes_id)
+
+    # DataFrame 으로 바꿔서 리턴
+    return pd.DataFrame(similar_indexes_id)
 
 
-# 결론
-# 영화 리스트를 넣어주면 추천 리스트를 반환!
-def content_based_tag(perform):
-    performances = pd.read_csv()  # csv 데이터 읽어올 곳 경로 작성
-    performances_df = performances[['id', 'tags']]  # 태그 기반 추천(tags는 나중에 맞는 이름으로 변경)
-    tags_sim_sorted_ind = pd.read_csv() # sim 계산한 거 불러 옴
-    recommend_list = find_sim_performances(perform, performances_df, tags_sim_sorted_ind)
-    return recommend_list
+def content_based_recommend(pid):
+    find_recommend(pid)
+
+
+content_based_tag_to_csv()
+# find_recommend()
+content_based_recommend(193)
