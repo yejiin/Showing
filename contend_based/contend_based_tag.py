83cfb10cdf51d848814858023e987a3657929593
고주희 <budludl@naver.com>
2022-03-23 15:21:45 +0900

    STYLE :  태그 기반 콘텐츠 필터링 주석 정리 [#S06P22A301-112]

--- contend_based/contend_based_tag.py
+++ contend_based/contend_based_tag.py
@@ -10,8 +10,9 @@ import json
 warnings.filterwarnings('ignore')
 
 
+# 공연 id - 태그 json 파일 읽어서 가공 - 저장하는 함수
 def content_based_tag_to_csv():
-    with open("C:/Users/SSAFY/Downloads/UI/test.json", "r", encoding="utf8") as f:
+    with open("C:/Users/SSAFY/Downloads/UI/test.json", "r", encoding="utf8") as f: # json 파일 경로
         json_data = json.load(f)
 
     print(json_data)
@@ -22,32 +23,26 @@ def content_based_tag_to_csv():
     # 0을 뺀 새로운 dictionary 만들기
     e = {}
     for i in range(len(keys)):
-        e[keys[i]] = json_data[keys[i]]['0'] # {'i': {'데스': 41.7049521733, '데스노트': 40.6126831601, '노트': 34.9861600762, '데': 23.2812831277, '스': 22.6366570209, '라이토': 20.3063415801, '미사': 16.9219513167, '컬': 15.9068400688, '뮤지컬': 15.2950385276, '뮤': 15.2950385276}}
+        e[keys[i]] = json_data[keys[i]]['0']
     print(e)
 
     # DataFrame 만들기
     e_df = pd.DataFrame.from_dict(e)
     # NaN을 0으로 바꾸기
     e_df = e_df.replace(np.nan, 0)
-    # print(e_df)
 
     # df 전치시키기
     e_df = pd.DataFrame.transpose(e_df)
-    # e_df.index(keys)
-    print(e_df)
-
 
     # 코사인 유사도 구하기
     tags_sim = cosine_similarity(e_df, e_df)
-    # print(tags_sim.shape)
     # print(tags_sim)
 
-    # 구한 거 정렬
+    # 코사인 유사도가 높은 순으로 정렬
     tags_sim_sort_ind = tags_sim.argsort()[:, ::-1]
-    # tags_sim_sort_ind.index(keys)
     print(tags_sim_sort_ind)
 
-    # 코사인 유사도 저장
+    # 정렬된 행렬 저장 index 는 공연 id
     path = "C:/Users/SSAFY/Downloads/UI/output.csv"  # 저장할 경로
     tag_csv = pd.DataFrame(tags_sim_sort_ind, index=keys)
     tag_csv.to_csv(path, header=True, index=True)
@@ -55,18 +50,18 @@ def content_based_tag_to_csv():
     pass
 
 
+# 유사도 높은 공연 저장한 파일 읽고 특정 공연으로 검색
 def find_recommend(pid) :
     # tags_sim_sort_ind 갖고옴 : index 는 공연 id
-    tags_sim_sort_ind = pd.read_csv('C:/Users/SSAFY/Downloads/UI/output.csv', index_col=0)
+    tags_sim_sort_ind = pd.read_csv('C:/Users/SSAFY/Downloads/UI/output.csv', index_col=0) # 행렬 저장된 경로
     # print(tags_sim_sort_ind)
 
     # 함수 호출
-    find_sim_performance(tags_sim_sort_ind, pid)
+    find_sim_performance_by_tag(tags_sim_sort_ind, pid)
 
 
-def find_sim_performance(sorted_ind, pid, top_n=16):
-    # performance_id 는 공연 id를 str 으로 변환한 값
-    performance_id = str(pid)
+# 특정 공연과 유사도가 높은 공연 top_n개 리턴
+def find_sim_performance_by_tag(sorted_ind, pid, top_n=16):
 
     # 원하는 개수만큼만 뽑기
     similar_indexes = sorted_ind.loc[pid, : str(top_n)]
@@ -85,10 +80,8 @@ def find_sim_performance(sorted_ind, pid, top_n=16):
     return pd.DataFrame(similar_indexes_id)
 
 
+# 전체 호출 함수 : pid 는 Integer
 def content_based_recommend(pid):
     find_recommend(pid)
 
 
-content_based_tag_to_csv()
-# find_recommend()
-content_based_recommend(193)
