4f6bcde729f61ab6684333e73557105c9b81670e
hyerin <lyyyn@naver.com>
2022-03-24 18:21:17 +0900

    FEAT: 메인페이지 틀 잡기

---
+++ frontend/src/views/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <show></show>
+    <actor></actor>
+    <ranking-musical></ranking-musical>
+    <ranking-theater></ranking-theater>
+    <rating-musical></rating-musical>
+    <rating-theater></rating-theater>
+  </div>
+</template>
+<script>
+import Show from "@/components/recommend/show";
+import Actor from "@/components/recommend/actor";
+import RankingMusical from "@/components/show/rankingMusical";
+import RankingTheater from "@/components/show/rankingTheater";
+import RatingMusical from "@/components/show/ratingMusical";
+import RatingTheater from "@/components/show/ratingTheater";
+
+export default {
+  name: "components",
+  components: {
+    Show,
+    Actor,
+    RankingMusical,
+    RankingTheater,
+    RatingMusical,
+    RatingTheater,
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  padding-top: 10%;
+  padding-left: 5%;
+  padding-right: 5%;
+}
+</style>
