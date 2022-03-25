0d83a750e8d58172511f7d856176e12c7c7e832f
hyerin <lyyyn@naver.com>
2022-03-25 21:49:01 +0900

    FEAT: 공연 상세 페이지 구현

---
+++ frontend/src/components/show/Comment.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <h5 class="main_title comment_title">다른 사람의 리뷰</h5>
+    <br />
+    <h6 class="more">더보기</h6>
+    <!-- carousel area -->
+    <b-card-group deck class="mb-0 comment_list">
+      <!-- 화살표 아이콘을 통해 슬라이딩 할 경우 -->
+      <i class="ni ni-bold-left arrow"></i>
+      <b-card
+        v-for="(item, index) in currentPageCards"
+        :key="index"
+        class="mr-0 mb-2"
+        tag="article"
+        style="max-width: 20rem"
+      >
+        <i style="font-size: 40px" class="ni ni-circle-08"></i>&nbsp;
+        <b-card-title class="comment_writer">작성자</b-card-title>
+        <hr />
+        <b-card-text>리뷰 내용</b-card-text>
+      </b-card>
+      <i class="ni ni-bold-right arrow arrow_right"></i>
+    </b-card-group>
+    <br />
+    <!-- pagination area -->
+    <!-- 페이징을 사용해서 슬라이딩 할 경우 (아래 js 참고 코드 있음) -->
+    <div class="pagination" v-if="cards.length > cardsPerPage">
+      <div class="index" v-for="i in pageCount" :key="i" @click="next(i)" :class="{ active: currentPage(i) }"></div>
+    </div>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Comment",
+  data() {
+    return {
+      cards: [
+        {
+          //Data in the card as objects
+        },
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+      ],
+      paginatedCards: [],
+      pageCount: 0,
+      cardsPerPage: 4,
+      currentPageIndex: 0,
+    };
+  },
+
+  computed: {
+    currentPageCards() {
+      this.createPages();
+
+      return this.paginatedCards[this.currentPageIndex];
+    },
+  },
+
+  methods: {
+    currentPage(i) {
+      return i - 1 === this.currentPageIndex;
+    },
+
+    createPages() {
+      let cardsLength = this.cards.length;
+      let fullPagesCount = Math.floor(cardsLength / this.cardsPerPage);
+
+      if (cardsLength > this.cardsPerPage) {
+        this.pageCount = 0;
+        for (let i = 0; i < fullPagesCount * this.cardsPerPage; i += this.cardsPerPage) {
+          this.paginatedCards[this.pageCount] = this.cards.slice(i, i + this.cardsPerPage);
+          this.pageCount++;
+        }
+
+        this.paginatedCards[this.pageCount] = this.cards.slice(cardsLength - this.cardsPerPage, cardsLength);
+        this.pageCount = this.pageCount + 1;
+      } else {
+        this.paginatedCards[0] = this.cards;
+      }
+    },
+
+    next(i) {
+      this.currentPageIndex = i - 1;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pagination {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+}
+
+.index {
+  margin-left: 10px;
+  width: 10px;
+  height: 10px;
+  background: #9badf6;
+}
+
+.active {
+  width: 13px;
+  height: 13px;
+}
+
+.arrow {
+  margin-top: 10%;
+  color: #9badf6;
+}
+
+.arrow_right {
+  margin-left: 1%;
+}
+
+.more {
+  color: #3f46d8;
+  float: right;
+}
+
+.comment_title {
+  float: left;
+}
+
+.comment_list {
+  clear: both;
+}
+
+.comment_writer {
+  display: inline-block;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  font-size: 120%;
+}
+
+.card-text {
+  color: black;
+}
+</style>
---
+++ frontend/src/components/show/ShowInfo.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <b-card>
+      <!-- <h5 class="main_title">공연정보</h5> -->
+      <b-dropdown variant="secondary" id="dropdown-1" text="공연정보" class="m-md-0">
+        <b-dropdown-item>시즌3</b-dropdown-item>
+        <b-dropdown-item>시즌2</b-dropdown-item>
+        <b-dropdown-item>시즌1</b-dropdown-item>
+      </b-dropdown>
+      <br />
+      <br />
+      <span class="badge badge-pill badge-warning">15세 이상 관람가</span>
+      <br /><br />
+      <b-card-text>일정</b-card-text>
+      <b-card-text class="location">장소</b-card-text>
+      <b-card-text>공연시간</b-card-text>
+      <actor-list></actor-list>
+    </b-card>
+    <br />
+    <br />
+  </div>
+</template>
+
+<script>
+import ActorList from "@/components/show/ActorList";
+
+export default {
+  name: "ShowInfo",
+  components: {
+    ActorList,
+  },
+};
+</script>
+
+<style scoped>
+.card-text {
+  margin-bottom: 0px;
+  color: black;
+}
+
+.more {
+  color: #3f46d8;
+  float: right;
+}
+
+.btn-secondary {
+  color: #212529;
+  background-color: white !important;
+  border-color: white !important;
+  -webkit-box-shadow: 0 4px 6px rgb(50 50 93 / 11%), 0 1px 3px rgb(0 0 0 / 8%);
+  box-shadow: 0 4px 6px rgb(50 50 93 / 11%), 0 1px 3px rgb(0 0 0 / 8%);
+}
+
+.location {
+  float: left;
+  margin-right: 50%;
+}
+</style>
