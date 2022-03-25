f69c877e9ceb3e1a7e09d8cede33805c2c4ab090
이명주 <myungju25@naver.com>
2022-03-25 09:45:01 +0900

    CHORE: Security Swagger 설정

--- backend/src/main/java/com/showing/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/showing/backend/config/SecurityConfig.java
@@ -1,21 +1,72 @@
 package com.showing.backend.config;
 
-import org.springframework.context.annotation.Configuration;
+import com.showing.backend.common.auth.JwtAccessDeniedHandler;
+import com.showing.backend.common.auth.JwtAuthenticationEntryPoint;
+import com.showing.backend.common.auth.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  private final JwtTokenProvider tokenProvider;
+  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+  private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .httpBasic();
-    }
+  public SecurityConfig(JwtTokenProvider tokenProvider,
+      JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+      JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+    this.tokenProvider = tokenProvider;
+    this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+    this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  public void configure(WebSecurity web) {
+    web.ignoring()
+        .antMatchers(
+            "/h2-console/**",
+            "/error",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/api/v1/**",
+            "/v2/api-docs");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .cors().and()
+        .csrf().disable()
+        // 에러 처리 핸들러 등록
+        .exceptionHandling()
+        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+        .accessDeniedHandler(jwtAccessDeniedHandler)
+        .and()
+
+        // 세션을 사용하지 않고 jwt 사용
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .apply(new JwtSecurityConfig(tokenProvider));
+  }
 
 }
