edcd7f4d6424c235879981e9e2b7bc51ce605dc0
이명주 <myungju25@naver.com>
2022-03-25 11:16:29 +0900

    REFACTOR: JPA entity 수정

--- backend/src/main/java/com/showing/backend/db/entity/performance/Casting.java
+++ backend/src/main/java/com/showing/backend/db/entity/performance/Casting.java
@@ -1,7 +1,7 @@
 package com.showing.backend.db.entity.performance;
 
 
-import com.showing.backend.db.entity.CreatedTimeEntity;
+import com.showing.backend.db.entity.BaseTimeEntity;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,18 +11,18 @@ import javax.persistence.*;
 @Setter
 @Entity
 @Table(name = "casting")
-public class Casting extends CreatedTimeEntity {
+public class Casting extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "season_id", referencedColumnName = "playdbId")
+    @JoinColumn(name = "season_playdb_id", referencedColumnName = "playdbId")
     private Season season;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "actor_id", referencedColumnName = "playdbId")
+    @JoinColumn(name = "actor_playdb_id", referencedColumnName = "playdbId")
     private Actor actor;
 
     @Column
--- backend/src/main/java/com/showing/backend/db/repository/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.showing.backend.db.repository;
-
-import com.showing.backend.db.entity.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-}
