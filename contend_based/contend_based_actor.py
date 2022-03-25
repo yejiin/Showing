fc68dcb74ab37a560b9620c6e723796980127418
고주희 <budludl@naver.com>
2022-03-25 14:02:11 +0900

    FEAT : 배우 코사인유사도 정렬 csv 저장[#S06P22A301-113]

--- contend_based/contend_based_actor.py
+++ contend_based/contend_based_actor.py
@@ -2,14 +2,7 @@ import pandas as pd
 import numpy as np
 import warnings;
 
-from sqlalchemy.sql.functions import percent_rank
-
-from ast import literal_eval
-from sklearn.feature_extraction.text import  CountVectorizer
 from sklearn.metrics.pairwise import  cosine_similarity
-import pyodbc
-from sqlite3 import connect
-import pymysql
 
 
 warnings.filterwarnings('ignore')
@@ -38,11 +31,7 @@ actors['cast'] = 1
 
 df_merge_col = pd.merge(df_merge_col, actors, left_on='playdb_id', right_on='season_playdb_id')
 df_merge_col = df_merge_col.drop_duplicates(ignore_index = True)
-print(df_merge_col)
-
-j = 86403
-print(df_merge_col[df_merge_col['playdb_id']==j])
-print(df_merge_col[df_merge_col['playdb_id']==j]['performance_id'])
+# print(df_merge_col)
 
 e = {}
 for i in df_merge_col['playdb_id']:
@@ -50,15 +39,25 @@ for i in df_merge_col['playdb_id']:
     e[df_merge_col[df_merge_col['playdb_id']==i]['performance_id'].tolist()[0]] = \
         actors[actors['season_playdb_id']==i]['cast'].to_dict()
 
-print(e)
-print(len(e))
+# print(e)
+# print(len(e))
 
 actors_df = pd.DataFrame.from_dict(e)
 actors_df = actors_df.replace(np.nan, 0)
-print("actors_df : ")
-print(actors_df)
-print(actors_df.keys())
-# print(actors_df[86403].tolist())
-# print(actors[actors['season_playdb_id']==86403])
+# print("actors_df : ")
+# print(actors_df)
 
+actors_df = actors_df.transpose()
+sim_actor = cosine_similarity(actors_df, actors_df)
+# print(sim_actor)
+
+# 코사인 유사도가 높은 순으로 정렬
+tags_sim_sort_ind = sim_actor.argsort()[:, ::-1]
+# print(tags_sim_sort_ind)
+
+# 정렬된 행렬 저장 index 는 공연 id
+keys = df_merge_col['performance_id'].tolist()
+path = "C:/Users/SSAFY/Downloads/UI/output_actor.csv"  # 저장할 경로
+tag_csv = pd.DataFrame(tags_sim_sort_ind, index=keys)
+tag_csv.to_csv(path, header=True, index=True)
 
