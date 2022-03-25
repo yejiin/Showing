a8072b35b9fb5e0ee1ce3395496baa33b63fd173
고주희 <budludl@naver.com>
2022-03-25 13:12:59 +0900

    REFACTOR : 시즌별 배우 df 완성[#S06P22A301-113]

--- contend_based/contend_based_actor.py
+++ contend_based/contend_based_actor.py
@@ -15,11 +15,7 @@ import pymysql
 warnings.filterwarnings('ignore')
 
 
-df = pd.read_csv('C:/Users/SSAFY/Downloads/UI/casting.csv', encoding='cp949', index_col=0)
-actors = df[['actor_id']]
-# print(actors)
-
-# csv to dataframe
+# csv to DataFrame
 df = pd.read_csv('C:/Users/SSAFY/Downloads/UI/performance.csv', encoding='cp949')
 # print(df.columns)
 performance = df[['id', 'last_season_id']]
@@ -29,10 +25,26 @@ df = pd.read_csv('C:/Users/SSAFY/Downloads/UI/season.csv', encoding='cp949')
 seasons = df[['id', 'playdb_id']]
 # print(seasons)
 
-# performance['playdb_id'] = seasons[seasons['id']==performance['last_season_id']]['playdb_id']
-# print(performance)
-
 df_merge_col = pd.merge(performance, seasons, left_on='last_season_id', right_on='id')
 df_merge_col.rename(columns={'id_x':'performance_id'}, inplace=True)
 df_merge_col = df_merge_col[['performance_id', 'last_season_id', 'playdb_id']]
-print(df_merge_col)
+print("df_merge_col : ")
+print(df_merge_col.shape)
+
+df = pd.read_csv('C:/Users/SSAFY/Downloads/UI/casting.csv', encoding='cp949', index_col='actor_playdb_id')
+actors = df[['season_playdb_id']]
+actors['cast'] = 1
+print(actors)
+
+e = {}
+for i in range(len(df_merge_col)):
+    e[df_merge_col['performance_id'][i]] = \
+        actors[actors['season_playdb_id']==df_merge_col['playdb_id'][i]]['cast'].to_dict()
+
+print(e)
+print(len(e))
+
+actors_df = pd.DataFrame.from_dict(e)
+actors_df = actors_df.replace(np.nan, 0)
+print(actors_df)
+
