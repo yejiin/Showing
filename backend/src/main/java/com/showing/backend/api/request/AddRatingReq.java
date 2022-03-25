721f574e962575beb54ba939f60fa1b9ad87f344
이명주 <myungju25@naver.com>
2022-03-25 20:35:47 +0900

    FIX: AddRatingReg SeasonId -> PerformanceId로 변경

--- backend/src/main/java/com/showing/backend/api/request/AddRatingReq.java
+++ backend/src/main/java/com/showing/backend/api/request/AddRatingReq.java
@@ -13,8 +13,8 @@ public class AddRatingReq {
     @ApiModelProperty(name = "유저의 Id", example = "1")
     private Long userId;
 
-    @ApiModelProperty(name = "공연의 시즌 Id", example = "1")
-    private Long seasonId;
+    @ApiModelProperty(name = "퍼포먼스 Id", example = "1")
+    private Long performanceId;
 
     @ApiModelProperty(name = "별점", example = "5")
     private int rating;
