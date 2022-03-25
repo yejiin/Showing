af39732a235b425752c0fb5e6850d8c918af5dfd
이명주 <myungju25@naver.com>
2022-03-25 21:16:45 +0900

    FEAT: InvalidException 에러 처리 추가

--- backend/src/main/java/com/showing/backend/common/exception/InvalidException.java
+++ backend/src/main/java/com/showing/backend/common/exception/InvalidException.java
@@ -3,7 +3,12 @@ package com.showing.backend.common.exception;
 import com.showing.backend.common.exception.handler.ErrorCode;
 
 public class InvalidException extends BusinessException {
+
     public InvalidException() {
         super(ErrorCode.INVALID_INPUT_VALUE);
+    }
+
+    public InvalidException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
     }
 }
