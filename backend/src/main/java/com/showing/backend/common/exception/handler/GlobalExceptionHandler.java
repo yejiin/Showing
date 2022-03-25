1b7be9c8f492ab4730c23b9f1f6dc36b00492791
김혜지 <hengzizng@naver.com>
2022-03-25 10:13:59 +0900

    FEAT: NotFoundException 추가

--- backend/src/main/java/com/showing/backend/common/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/showing/backend/common/exception/handler/GlobalExceptionHandler.java
@@ -1,9 +1,8 @@
 package com.showing.backend.common.exception.handler;
 
-import com.showing.backend.common.exception.*;
-import javassist.NotFoundException;
+import com.showing.backend.common.exception.BusinessException;
+import com.showing.backend.common.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,27 +21,12 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * Value Duplicate Exception 처리
-     */
-    @ExceptionHandler(DuplicateException.class)
-    public ResponseEntity<ErrorResponse> duplicateException(DuplicateException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(ErrorCode.DUPLICATE_INPUT_VALUE));
-    }
-
-    /**
-     * Value Invalid Exception 처리
-     */
-    @ExceptionHandler(InvalidException.class)
-    public ResponseEntity<ErrorResponse> invalidException(InvalidException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE));
-    }
-
-    /**
      * Entity Not Found Exception 처리
      */
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> entityNotFoundException(NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(ErrorCode.ENTITY_NOT_FOUND));
+    public ResponseEntity<ErrorResponse> notFoundException(NotFoundException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity.status(errorCode.getStatus()).body(ErrorResponse.of(errorCode));
     }
 
 }
