a252668cbe7f919e8f0aa43f655fbff117b7ffc6
hyerin <lyyyn@naver.com>
2022-03-25 21:49:01 +0900

    FEAT: 공연 상세 페이지 구현

---
+++ frontend/src/components/show/Story.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-card>
+      <h5 class="main_title">줄거리</h5>
+      <b-card-text>줄거리 내용</b-card-text>
+      <h6 class="more">더보기</h6>
+    </b-card>
+    <br />
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Story",
+};
+</script>
+
+<style scoped>
+.card-text {
+  margin-bottom: 0px;
+  color: black;
+}
+
+.more {
+  color: #3f46d8;
+  float: right;
+}
+</style>
