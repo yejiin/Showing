82ff4eed48fd52f2bb9ae1dc2641463eb9455b3f
이명주 <myungju25@naver.com>
2022-03-28 00:02:59 +0900

    FEAT: 선호 배우 조회 쿼리 QueryDsl 구현

---
+++ backend/src/main/java/com/showing/backend/db/repository/recommend/FavoriteActorRepositoryCustomImpl.java
@@ -0,0 +1,32 @@
+package com.showing.backend.db.repository.recommend;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.showing.backend.api.response.FavActorRes;
+import com.showing.backend.db.entity.User;
+import com.showing.backend.db.entity.performance.QActor;
+import com.showing.backend.db.entity.recommend.QFavoriteActor;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class FavoriteActorRepositoryCustomImpl implements FavoriteActorRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    QActor qActor = QActor.actor;
+    QFavoriteActor qFavoriteActor = QFavoriteActor.favoriteActor;
+
+    @Override
+    public Optional<List<FavActorRes>> getFavActorListByUser(User user) {
+        List<FavActorRes> res = jpaQueryFactory.select(Projections.constructor(FavActorRes.class,qActor.actorName,qActor.actorName))
+                .from(qActor).join(qFavoriteActor)
+                .on(qActor.eq(qFavoriteActor.actor))
+                .where(qFavoriteActor.user.eq(user))
+                .fetch();
+
+        return Optional.ofNullable(res);
+    }
+}
