0b35bec16651b850bc7b69560cc8bd03f7f60c7a
hyerin <lyyyn@naver.com>
2022-03-28 10:44:58 +0900

    CHORE: 백엔드 api 호출을 위한 코드 추가

--- frontend/src/api/review.js
+++ frontend/src/api/review.js
@@ -2,4 +2,20 @@ import { apiInstance } from "./index.js";
 
 const api = apiInstance();
 
-export {};
+function listMyReview(userId, success, fail) {
+  api.get(`/reviews/users/${userId}`).then(success).catch(fail);
+}
+
+function addMyReview(review, success, fail) {
+  api.post(`/reviews`, JSON.stringify(review)).then(success).catch(fail);
+}
+
+function deleteMyReview(reviewId, success, fail) {
+  api.delete(`/reviews/${reviewId}`).then(success).catch(fail);
+}
+
+function modifyMyReview(reviewId, review, success, fail) {
+  api.put(`/reviews/${reviewId}`, JSON.stringify(review)).then(success).catch(fail);
+}
+
+export { listMyReview, addMyReview, deleteMyReview, modifyMyReview };
