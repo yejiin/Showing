d7078e397afe45ac993584988f790db58f90f378
이명주 <myungju25@naver.com>
2022-03-28 23:05:22 +0900

    FIX: last_season_id 수정중

--- crawling/db.py
+++ crawling/db.py
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 db_connection_str = (
-    "mysql+pymysql://root:" + "ssafy" + "@127.0.0.1:3306/showing?charset=utf8"
+    "mysql+pymysql://ssafy:" + "a301showing" + "@j6a301.p.ssafy.io:3306/test?charset=utf8"
 )
 db_connection = create_engine(db_connection_str)
 conn = db_connection.connect()
@@ -29,28 +29,32 @@ def insert_on_duplicate(table, conn, keys, data_iter):
 
 # query = "SELECT s.interpark_id, s.playdb_id, p.performance_name FROM season s join performance p on s.performance_id=p.id where p.performance_type = 2 and s.interpark_id is not null"
 
+# query = "SELECT * from season"
 # season_db = pd.read_sql_query(query, conn)
-# season_db.to_csv(r"./data/playonly.csv", index=False, encoding="utf-8-sig")
+# season_db.to_csv(r"./seasons.csv", index=False, encoding="utf-8-sig")
 
-df_all = pd.read_csv("./data/castings_play.csv")
+df_all = pd.read_csv("./seasons_noid.csv")
 
 start_time = time.time()
 
+# df_all['id']=None
+
+# df_all.to_csv("./seasons_noid.csv",index=False, encoding="utf-8-sig",  header=True)
+
 # 로우 별 db 저장
-for i in range(0, len(df_all.index) + 1):
-    df = df_all.iloc[i : i + 1]
-    try:
+# for i in range(0, len(df_all.index) + 1):
+df = df_all.iloc[0:1]
+try:
         df.to_sql(
-            name="casting",
+            name="season",
             con=db_connection,
             if_exists="append",
-            chunksize=10000,
             index=False,
             method="multi",
         )
-    except Exception as e:
-        print("에러 발생 : ")
-        print(df)
+except Exception as e:
+    print("에러 발생 : ")
+    print(df)
 
 # 전체 db 저장
 # try:
