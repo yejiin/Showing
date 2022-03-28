fa4edbdf5d0a21d9d1c8f551f951173cb45e2602
hyerin <lyyyn@naver.com>
2022-03-28 15:22:55 +0900

    FEAT: 검색창 내의 자동완성

--- frontend/src/components/search/Search.vue
+++ frontend/src/components/search/Search.vue
@@ -1,11 +1,33 @@
 <template>
-  <base-input class="searchInput" placeholder="공연과 배우를 검색해보세요" addon-left-icon="ni ni-zoom-split-in">
-  </base-input>
+  <vue-simple-suggest
+    placeholder="공연과 배우를 검색해보세요"
+    class="searchInput"
+    min-length="0"
+    v-model="chosen"
+    :list="showAndActorList"
+    :filter-by-query="true"
+  >
+  </vue-simple-suggest>
 </template>
 
 <script>
+import VueSimpleSuggest from "vue-simple-suggest";
+import "vue-simple-suggest/dist/styles.css"; // Optional CSS
+
 export default {
-  name: "Search",
+  components: {
+    VueSimpleSuggest,
+  },
+  data() {
+    return {
+      chosen: "",
+    };
+  },
+  methods: {
+    showAndActorList() {
+      return ["지킬 앤 하이드", "지킬 앤 하이도", "자킬 앤 하이드"];
+    },
+  },
 };
 </script>
 
@@ -13,5 +35,6 @@ export default {
 .searchInput {
   margin-bottom: 0px !important;
   margin-right: 5%;
+  width: 120%;
 }
 </style>
