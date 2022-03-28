ae9b0e100596d480626711bd0836c55ee3b18ed3
hyerin <lyyyn@naver.com>
2022-03-28 10:44:58 +0900

    CHORE: 백엔드 api 호출을 위한 코드 추가

--- frontend/src/api/show.js
+++ frontend/src/api/show.js
@@ -2,4 +2,20 @@ import { apiInstance } from "./index.js";
 
 const api = apiInstance();
 
-export {};
+function listSeasonShow(performanceId, success, fail) {
+  api.get(`/performances/seasons/list/${performanceId}`).then(success).catch(fail);
+}
+
+function detailSeasonShow(seasonId, success, fail) {
+  api.get(`/performances/seasons/${seasonId}`).then(success).catch(fail);
+}
+
+function listShow(success, fail) {
+  api.get(`/performances`).then(success).catch(fail);
+}
+
+function detailShow(performanceId, success, fail) {
+  api.get(`/performances/${performanceId}`).then(success).catch(fail);
+}
+
+export { listSeasonShow, detailSeasonShow, listShow, detailShow };
--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -2,4 +2,12 @@ import { apiInstance } from "./index.js";
 
 const api = apiInstance();
 
-export {};
+async function getUser(userId, success, fail) {
+  await api.get(`/users/${userId}`).then(success).catch(fail);
+}
+
+async function modifyUser(user, success, fail) {
+  await api.put(`/users`, JSON.stringify(user)).then(success).catch(fail);
+}
+
+export { getUser, modifyUser };
