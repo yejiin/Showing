4889c884bc127b7f368cac50e4f357fbd53c116c
김혜지 <hengzizng@naver.com>
2022-03-28 11:49:07 +0900

    FEAT: 리뷰 삭제 시 선호 배우 가중치 감소 기능 구현

--- backend/src/main/java/com/showing/backend/api/service/ActorService.java
+++ backend/src/main/java/com/showing/backend/api/service/ActorService.java
@@ -1,9 +1,15 @@
 package com.showing.backend.api.service;
 
 import com.showing.backend.api.response.ActorRes;
+import com.showing.backend.db.entity.User;
+import com.showing.backend.db.entity.performance.Actor;
 
 import java.util.List;
 
 public interface ActorService {
+
     List<ActorRes> getSeasonCastingList(Long seasonPlaydbId);
+
+    void setFavoriteActorWeight(int weight, User user, Actor actor);
+
 }
