6bb44da42c2566e9d3dfac407fbf98cde2c1b984
이명주 <myungju25@naver.com>
2022-03-28 17:43:08 +0900

    FEAT: 별점 추가,수정,삭제 시 선호 태그 가중치 값 변경 기능 추가

--- backend/src/main/java/com/showing/backend/db/repository/PerformanceTagRepository.java
+++ backend/src/main/java/com/showing/backend/db/repository/PerformanceTagRepository.java
@@ -1,7 +1,18 @@
 package com.showing.backend.db.repository;
 
 import com.showing.backend.db.entity.PerformanceTag;
+import com.showing.backend.db.entity.Tag;
+import com.showing.backend.db.entity.performance.Performance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PerformanceTagRepository extends JpaRepository<PerformanceTag, Long> {
+
+    /**
+     * tag table 에서 performanceId 로 해당 performance의 태그를 모두 조회한다.
+     */
+    List<PerformanceTag> findAllByPerformance(Performance performance);
 }
