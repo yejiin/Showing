1002d40c4dddfc76cbd8b7737bd04c743a47242e
hyerin <lyyyn@naver.com>
2022-03-29 21:51:57 +0900

    FEAT: 공연 정보 api 연결

--- frontend/src/components/show/ShowInfo.vue
+++ frontend/src/components/show/ShowInfo.vue
@@ -13,32 +13,72 @@
           공연정보
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item">시즌3</a>
-          <a class="dropdown-item">시즌2</a>
-          <a class="dropdown-item">시즌1</a>
+          <!-- <a v-for="(seasonDate, index) in seasonDates" :key="index" class="dropdown-item">
+            {{ seasonDate[index].startDate }} ~ {{ seasonDate[index].endDate }}
+          </a> -->
+          <a class="dropdown-item"> {{ seasonDates[0].startDate }} ~ {{ seasonDates[0].endDate }} </a>
         </div>
       </div>
       <br />
       <br />
-      <span class="badge badge-pill badge-warning">15세 이상 관람가</span>
+      <span class="badge badge-pill badge-success">{{ info.detailType }}</span
+      >&nbsp;
+      <span class="badge badge-pill badge-warning">{{ info.performanceAge }} 관람가</span>
       <br /><br />
-      <b-card-text>일정</b-card-text>
-      <b-card-text class="location">장소</b-card-text>
-      <b-card-text>공연시간</b-card-text>
-      <actor-list></actor-list>
+      <b-row
+        ><b-col
+          ><b-card-text>일정 {{ info.startDate }} ~ {{ info.endDate }}</b-card-text></b-col
+        ></b-row
+      >
+      <b-row
+        ><b-col cols="6"
+          ><b-card-text class="location">장소 {{ info.location }}</b-card-text></b-col
+        >
+        <b-col cols="6">
+          <b-card-text>공연시간 {{ info.runingTime }}</b-card-text></b-col
+        ></b-row
+      >
+      <actor-list :actor="actor"></actor-list>
     </b-card>
     <br />
     <br />
+    <story :description="description"></story>
   </div>
 </template>
 
 <script>
 import ActorList from "@/components/show/ActorList";
+import Story from "@/components/show/Story";
+import { getSeasonShow } from "@/api/show.js";
 
 export default {
   name: "ShowInfo",
   components: {
     ActorList,
+    Story,
+  },
+  props: {
+    info: Object,
+    actor: Array,
+    description: String,
+  },
+  data() {
+    return {
+      showInfo: {},
+      seasonDates: [],
+    };
+  },
+  async created() {
+    await getSeasonShow(
+      "911",
+      (response) => {
+        this.seasonDates = response.data.data;
+        // console.log(this.seasonDates);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   },
 };
 </script>
@@ -66,10 +106,5 @@ export default {
 .btn-white:hover {
   -webkit-box-shadow: 0 0px 0px, 0 0px 0px;
   box-shadow: 0 0px 0px, 0 0px 0px;
-}
-
-.location {
-  float: left;
-  margin-right: 50%;
 }
 </style>
