7f25604acef0bb27372fea9f6f657ba78601226b
hyerin <lyyyn@naver.com>
2022-03-30 01:40:07 +0900

    FEAT: 사용자 맞춤, 평균 별점 순위별 공연 추천 api 연결

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -2,36 +2,65 @@
   <div class="header">
     <h5 class="main_title">00님을 위한 추천 공연</h5>
     <br />
-    <show></show>
+    <show :mainRecommendList="mainRecommendList"></show>
     <h5 class="main_title">선호 배우 000의 다른 공연</h5>
     <br />
     <actor></actor>
-    <h5 class="main_title">현재 뮤지컬 예매율 순위</h5>
     <br />
-    <ranking-musical></ranking-musical>
-    <h5 class="main_title">현재 연극 예매율 순위</h5>
-    <br />
-    <ranking-theater></ranking-theater>
     <h5 class="main_title">뮤지컬 평균 별점 순위</h5>
     <br />
-    <rating-musical></rating-musical>
+    <rating-musical :musicalList="musicalList"></rating-musical>
     <h5 class="main_title">연극 평균 별점 순위</h5>
     <br />
-    <rating-theater></rating-theater>
+    <rating-play :playList="playList"></rating-play>
   </div>
 </template>
 <script>
+import Actor from "@/components/recommend/Actor";
 import Show from "@/components/recommend/Show";
+import RatingMusical from "@/components/recommend/RatingMusical";
+import RatingPlay from "@/components/recommend/RatingPlay";
+
+import { getMainRecommend } from "@/api/recommend.js";
+import { getRankingShow } from "@/api/show.js";
 
 export default {
   name: "MainPage",
   components: {
-    Show: Show,
-    Actor: Show,
-    RankingMusical: Show,
-    RankingTheater: Show,
-    RatingMusical: Show,
-    RatingTheater: Show,
+    Show,
+    Actor,
+    RatingMusical,
+    RatingPlay,
+  },
+  data() {
+    return {
+      musicalList: [],
+      playList: [],
+      mainRecommendList: [],
+    };
+  },
+  async created() {
+    await getRankingShow(
+      (response) => {
+        this.musicalList = response.data.data.musicalAvgList;
+        this.playList = response.data.data.playAvgList;
+        // console.log(response.data.data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    await getMainRecommend(
+      "1",
+      (response) => {
+        this.mainRecommendList = response.data.data;
+        // console.log(response.data.data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   },
 };
 </script>
