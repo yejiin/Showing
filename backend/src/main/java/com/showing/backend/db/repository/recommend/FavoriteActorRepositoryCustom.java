ee9adb3391bcc3ec5fba9605b539d58df133c300
김혜지 <hengzizng@naver.com>
2022-03-30 03:23:35 +0900

    REFACTOR: 선호 배우 조회 반환값 id -> Actor Entity 로 변경

--- backend/src/main/java/com/showing/backend/db/repository/recommend/FavoriteActorRepositoryCustom.java
+++ backend/src/main/java/com/showing/backend/db/repository/recommend/FavoriteActorRepositoryCustom.java
@@ -2,6 +2,7 @@ package com.showing.backend.db.repository.recommend;
 
 import com.showing.backend.api.response.FavActorRes;
 import com.showing.backend.db.entity.User;
+import com.showing.backend.db.entity.performance.Actor;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,6 @@ public interface FavoriteActorRepositoryCustom {
     /**
      * favorite_actor테이블에서 해당 유저의 가중치 높은 count개 actor id 를 조회한다.
      */
-    List<Long> findTopCountByUserId(int count, Long userId);
+    List<Actor> findTopCountByUserId(int count, Long userId);
 
 }
