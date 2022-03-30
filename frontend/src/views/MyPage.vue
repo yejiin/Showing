e6a60820cb1c8332aa81229732b1fd157a965007
yejiin <yj_song67@naver.com>
2022-03-30 18:56:38 +0900

    FEAT: 마이페이지 회원 정보 연동 [#S06P22A301-196]

--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <profile></profile>
+    <profile :profile="profile"></profile>
     <user-rating></user-rating>
     <user-favorite></user-favorite>
     <user-review></user-review>
@@ -12,6 +12,8 @@ import UserRating from "@/components/user/UserRating";
 import UserFavorite from "@/components/user/UserFavorite";
 import UserReview from "@/components/user/UserReview";
 
+import { getUserInfo } from "@/api/user.js";
+
 export default {
   name: "MyPage",
   components: {
@@ -19,6 +21,30 @@ export default {
     UserRating,
     UserFavorite,
     UserReview,
+  },
+  data() {
+    return {
+      profile: {
+        nickName: "",
+        email: "",
+        introduce: "",
+        userImage: "",
+      },
+    };
+  },
+  created() {
+    getUserInfo(
+      this.$route.params.userId,
+      (response) => {
+        this.profile.nickName = response.data.data.nickName;
+        this.profile.email = response.data.data.email;
+        this.profile.introduce = response.data.data.introduce;
+        this.profile.userImage = response.data.data.userImage;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   },
 };
 </script>
