89645d0f788244c941a1cd3cccaddc390a11653f
이명주 <myungju25@naver.com>
2022-03-31 01:07:10 +0900

    FEAT: 공연 상세 불러오기 구현 [#S06P22A301-221]

--- frontend/src/api/recommend.js
+++ frontend/src/api/recommend.js
@@ -6,8 +6,4 @@ function getMainRecommend(userId, success, fail) {
   api.get(`/recommend/users/${userId}`).then(success).catch(fail);
 }
 
-function getSimilarRecommend(performanceId, success, fail) {
-  api.get(`/recommend/${performanceId}`).then(success).catch(fail);
-}
-
-export { getMainRecommend, getSimilarRecommend };
+export { getMainRecommend };
