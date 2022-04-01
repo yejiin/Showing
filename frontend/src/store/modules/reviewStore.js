d3007e815ab950c03d9bea8c56a7d0c4537f8d67
yejiin <yj_song67@naver.com>
2022-04-02 02:43:20 +0900

    FIX: review store conflict

--- frontend/src/store/modules/reviewStore.js
+++ frontend/src/store/modules/reviewStore.js
@@ -1,84 +1,81 @@
-import { getNaverToken, getNaverUser, getKakaoToken, getKakaoUser } from "@/api/user.js";
-
 const reviewStore = {
   namespaced: true,
   state: {
-      modals:{
-          myReviewList : false,
-          writeReview : false,
-          OtherReviews : false,
-          reviewDetail : false,
-          modifyReview : false,
-      },
-      reviewInfo:{
-        reviewId : 0,
-      }
+    modals: {
+      myReviewList: false,
+      writeReview: false,
+      OtherReviews: false,
+      reviewDetail: false,
+<<<<<<< Updated upstream
+      modifyReview: false,
+      myReview: false,
+    },
+    reviewInfo: {
+      reviewId: 0,
+    },
+=======
+      myReview: false,
+    },
+>>>>>>> Stashed changes
   },
 
-  getters: {
-      
-  },
+  getters: {},
 
   mutations: {
-    SET_MY_REVIEW_LIST_STATE : (state, status) => {
-        state.modals.myReviewList = status
-        console.log("mutations "+status)
-        console.log(state.modals.myReviewList)
+    SET_MY_REVIEW_LIST_STATE: (state, status) => {
+      state.modals.myReviewList = status;
+      console.log("mutations " + status);
+      console.log(state.modals.myReviewList);
     },
-    SET_WRITE_REVIEW_STATE : (state, status) =>{
-      state.modals.writeReview = status
-      console.log("mutations "+status)
-      console.log(state.modals.writeReview)
+    SET_WRITE_REVIEW_STATE: (state, status) => {
+      state.modals.writeReview = status;
+      console.log("mutations " + status);
+      console.log(state.modals.writeReview);
     },
-    SET_MODIFY_REVIEW_STATE : (state, status) =>{
-      state.modals.modifyReview = status
-      console.log("mutations "+status)
-      console.log(state.modals.modifyReview)
+    SET_REVIEW_STATE: (state, status) => {
+      state.modals.myReview = status;
+      console.log("mutations " + status);
+      console.log(state.modals.myReview);
     },
-    SET_REVIEW_ID : (state, id) =>{
-      state.reviewInfo.reviewId = id
-      console.log("mutations "+id)
-      console.log(state.reviewInfo.reviewId)
-    }
-
+<<<<<<< Updated upstream
+    SET_MODIFY_REVIEW_STATE: (state, status) => {
+      state.modals.modifyReview = status;
+      console.log("mutations " + status);
+      console.log(state.modals.modifyReview);
+    },
+    SET_REVIEW_ID: (state, id) => {
+      state.reviewInfo.reviewId = id;
+      console.log("mutations " + id);
+      console.log(state.reviewInfo.reviewId);
+    },
+=======
+>>>>>>> Stashed changes
   },
 
   actions: {
-    setMyReviewListModalState({ commit }, status){
-        console.log("actions도 옴" + status)
-        commit("SET_MY_REVIEW_LIST_STATE", status);
+    setMyReviewListModalState({ commit }, status) {
+      console.log("actions도 옴" + status);
+      commit("SET_MY_REVIEW_LIST_STATE", status);
     },
-    setWriteReviewModalState({commit}, status){
-      console.log("actions도 옴" + status)
-        commit("SET_WRITE_REVIEW_STATE", status);
+    setWriteReviewModalState({ commit }, status) {
+      console.log("actions도 옴" + status);
+      commit("SET_WRITE_REVIEW_STATE", status);
     },
-    setModifyReviewModalState({commit}, status){
-      console.log("actions도 옴" + status)
-        commit("SET_MODIFY_REVIEW_STATE", status);
+<<<<<<< Updated upstream
+    setModifyReviewModalState({ commit }, status) {
+      console.log("actions도 옴" + status);
+      commit("SET_MODIFY_REVIEW_STATE", status);
     },
-    setReviewId({commit}, id){
-      console.log("actions도 옴" + id)
-        commit("SET_REVIEW_ID", id);
+    setReviewId({ commit }, id) {
+      console.log("actions도 옴" + id);
+      commit("SET_REVIEW_ID", id);
     },
-    async findNaverToken({ dispatch }, code, state) {
-      await getNaverToken(
-        code,
-        state,
-        (response) => {
-          if (response.data.statusCode == 200) {
-            console.log("네이버 Access Token 생성 성공");
-            console.log(response);
-
-            dispatch("findNaverUser", response.data.data);
-          }
-        },
-        (error) => {
-          console.log("네이버 Access Token 생성 실패");
-          console.log(error);
-        }
-      );
+=======
+>>>>>>> Stashed changes
+    setReviewModalState({ commit }, status) {
+      console.log("actions도 옴" + status);
+      commit("SET_REVIEW_STATE", status);
     },
-
   },
 };
 
