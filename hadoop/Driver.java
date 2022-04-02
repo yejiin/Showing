4ec47b6147521b5a16167babc15137b246f42bba
김혜지 <hengzizng@naver.com>
2022-04-03 02:17:26 +0900

    FEAT: wordcount 실행을 위한 Driver.java 작성

---
+++ hadoop/Driver.java
@@ -0,0 +1,22 @@
+package ssafy;
+
+import org.apache.hadoop.util.ProgramDriver;
+
+public class Driver {
+	public static void main(String[] args) {
+		int exitCode = -1;
+		ProgramDriver pgd = new ProgramDriver();
+		try {
+
+			pgd.addClass("wordcount", Wordcount.class, "A map/reduce program that performs word counting.");
+
+      		pgd.driver(args);
+			exitCode = 0;
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+
+		System.exit(exitCode);
+	}
+}
