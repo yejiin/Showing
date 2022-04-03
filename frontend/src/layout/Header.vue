92a3dcef1fb58c8249678e45c141a3170f48663c
고주희 <budludl@naver.com>
2022-04-03 16:09:22 +0900

    FEAT : 로그인 시 프로필 사진 드롭다웃으로 로그아웃과 마이페이지

--- frontend/src/layout/Header.vue
+++ frontend/src/layout/Header.vue
@@ -6,14 +6,20 @@
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <search></search>
-      <login style="margin-right: 15%"></login>
-      <p v-if="userInfo.userId">{{ userInfo.nickName }}님이 로그인하셨습니다.</p>
+      <login v-if="userInfo.userId==''" style="margin-right: 15%"></login>
+      <div class="dropdown">
+        <p v-if="userInfo.userId"><img class="profile" :src="userInfo.userImage" alt=""></p>
+        <div class="dropdown-content">
+          <p @click="HandleLogout">로그아웃</p>
+          <p @click="goToMyPage">마이페이지</p>
+        </div>
+      </div>
     </b-collapse>
   </b-navbar>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapActions, mapState } from "vuex";
 import Search from "@/components/search/Search.vue";
 import Login from "@/components/user/LoginModal.vue";
 
@@ -31,9 +37,18 @@ export default {
   },
 
   methods: {
+    ...mapActions(userStore, ["logout"]),
     goToMain() {
       this.$router.push({ name: "MainPage" });
     },
+    goToMyPage(){
+      this.$router.push({ path: '/users/'+this.userInfo.userId})
+    },
+    HandleLogout(){
+      this.logout()
+      alert("로그아웃 되었습니다")
+      this.goToMain()
+    }
   },
 };
 </script>
@@ -56,4 +71,23 @@ export default {
 .custnavi {
   border-bottom: solid 0.5px #e5e8eb;
 }
+.profile {
+  border-radius: 70%;
+  width: 40%;
+  margin-top: 20%;
+  /* margin-top: 20px; */
+  margin-left: 30px;
+}
+.dropdown { position: relative; display: inline-block; }
+.dropdown-content {
+  display: none;
+  position: absolute;
+  margin-left: 20%;
+  width: 160px;
+  /* background-color: #F9F9F9;
+  min-width: 160px;
+  padding: 8px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); */
+}
+.dropdown:hover .dropdown-content { display: block; }
 </style>
