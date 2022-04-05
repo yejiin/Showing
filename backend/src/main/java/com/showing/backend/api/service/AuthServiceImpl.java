74c6e10428c644999171869490e054be3779d1e8
이명주 <myungju25@naver.com>
2022-04-05 11:06:09 +0900

    FIX: callback uri 변경

--- backend/src/main/java/com/showing/backend/api/service/AuthServiceImpl.java
+++ backend/src/main/java/com/showing/backend/api/service/AuthServiceImpl.java
@@ -40,7 +40,7 @@ public class AuthServiceImpl implements AuthService {
 
         String param = "grant_type=authorization_code" +
                 "&client_id=" + KAKAO_CLIENT_ID +
-                "&redirect_uri=http://localhost:8080/callback" +
+                "&redirect_uri=https://j6a301.p.ssafy.io/callback" +
                 "&code=" + code;
         return getAccessToken(reqURL, param);
     }
